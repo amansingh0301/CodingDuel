f4787a7a201bb555f48206343c78eddad70e89fb
amansingh0301 <as9997450@gmail.com>
2021-02-01 00:00:19 +0530

    fifth commit

--- main.js
+++ main.js
@@ -37,9 +37,9 @@ app.use((req, res, next) => {
 });
 
 if(process.env.NODE_ENV === 'production'){
-  app.use(express.static(path.join(__dirname,'../client/public')))
+  app.use(express.static(path.join(__dirname,'./client/public')))
   app.get('*',(req,res)=>{
-    res.sendFile(path.join(__dirname,'../client/public','index.html'));
+    res.sendFile(path.join(__dirname,'./client/public','index.html'));
   })
 }
 app.use(express.json());
