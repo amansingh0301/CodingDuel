1ac8891decdc88cd7aa6b06d24f0cba8c074b2f8
amansingh0301 <as9997450@gmail.com>
2021-02-02 18:10:47 +0530

    main file changed

--- main.js
+++ main.js
@@ -40,10 +40,13 @@ app.use((req, res, next) => {
 //   app.get('*',(req,res)=>{
 //     res.sendFile(path.join(__dirname,'client','public','index.html'));
 //   })
-app.use(express.static(path.join(__dirname,'client/build')))
+
+if(process.env.NODE_ENV === 'production'){
+  app.use(express.static(path.join(__dirname,'client/build')))
   app.get('*',(req,res)=>{
     res.sendFile(path.join(__dirname,'client/build','index.html'));
   })
+}
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.raw());
