1e01f1d5b65a3fded59470a7b20e60af43925c67
amansingh0301 <as9997450@gmail.com>
2021-02-02 17:41:35 +0530

    * path

--- main.js
+++ main.js
@@ -40,13 +40,10 @@ app.use((req, res, next) => {
 //   app.get('*',(req,res)=>{
 //     res.sendFile(path.join(__dirname,'client','public','index.html'));
 //   })
-
-if(process.env.NODE_ENV === 'production'){
-  app.use(express.static(path.join(__dirname,'client/build')))
+app.use(express.static(path.join(__dirname,'client/build')))
   app.get('*',(req,res)=>{
     res.sendFile(path.join(__dirname,'client/build','index.html'));
   })
-}
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.raw());
