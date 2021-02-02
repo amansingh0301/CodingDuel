a4334b5e2d91fb8fa0b8bb0b3b27640f8f2709b7
amansingh0301 <as9997450@gmail.com>
2021-02-02 20:32:12 +0530

    testing

--- main.js
+++ main.js
@@ -43,12 +43,12 @@ app.use((req, res, next) => {
 
 if (process.env.NODE_ENV === 'production') {
   // Exprees will serve up production assets
-  app.use(express.static('client','build'));
+  app.use(express.static(__dirname,'client','build'));
 
   // Express serve up index.html file if it doesn't recognize route
   // const path = require('path');
   app.get('*', (req, res) => {
-    res.sendFile('client','build','index.html');
+    res.sendFile(__dirname,'client','build','index.html');
   });
 }
 
